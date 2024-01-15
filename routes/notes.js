db748e68cfcc4ad37d5c4879cc987cb31bfddbf7
ab <ab@my-mac-0.local>
2024-01-16 01:06:00 +0900

    install mongodb and update notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,33 +1,20 @@
 var express = require('express')
 var router = express.Router()
 
-// レスポンスのデータ (ノート全件)
-const responseObjectDataAll = {
-    textObject1: {
-        id: 1,
-        title: 'ノート1のタイトルです',
-        subTitle: 'ノート1のサブタイトルです',
-        bodyText: 'ノート1の本文です'
-    },
-    textObject2: {
-        id: 2,
-        title: 'ノート2のタイトルです',
-        subTitle: 'ノート2のサブタイトルです',
-        bodyText: 'ノート2の本文です'
-    }
-}
+const { MongoClient } = require('mongodb')
+const uri = "mongodb+srv://oizuiadsxapjbazmebyi:nAUHJraWUrYsAeJjAnBnxPwRE7YGAjzfez2c3Gsx@cluster0.4lozqs7.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(uri)
 
-/**
- * メモを全取得するAPI
- * @returns {Object[]} data
- * @returns {number} data.id - ID
- * @returns {string} data.title - タイトル
- * @returns {string} data.text - 内容
- */
+router.get('/', async (req, res) => {
+    // データベース、コレクションを指定
+    const database = client.db('notes')
+    const notes = database.collection('notes')
 
-router.get('/', function (req, res, next) {
-    // 全件取得して返す
-    res.json(responseObjectDataAll)
+    // idが1のドキュメントを取得
+    const query = { id: 2 }
+    const note = await notes.findOne(query)
+
+    res.json(note.title)
 })
 
 module.exports = router
