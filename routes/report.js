91f735620e46c3e26900b6ca3a8368a5808435a8
ab <ab@my-mac-0.local>
2024-01-16 03:22:59 +0900

    remove debugging code

--- routes/report.js
+++ routes/report.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     https.get('https://yesno.wtf/api', (response) => {
         response.on('data', (d) => {
             const json = JSON.parse(d.toString('utf8'))
-            console.log(json.answer)
+            // console.log(json.answer)
             res.render('report', { title: 'report2101100142', answer: json.answer, imgSrc: json.image })
         })
     }).on('error', (e) => {
