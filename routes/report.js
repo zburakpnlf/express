34feec6cb5cd28e761df3d8bfe4f8ca6f60564fc
ab <ab@my-mac-0.local>
2024-01-16 03:20:10 +0900

    update request for api

--- routes/report.js
+++ routes/report.js
@@ -1,19 +1,19 @@
 var express = require('express')
 var router = express.Router()
+const https = require('https')
 
 
-router.get('/', async (req, res) => {
-    const response = await fetch("https://yesno.wtf/api")
-    const yesno = await response.json()
-    /*
-        {
-            answer: yes/no
-            forced: bool
-            image: link
-        }
-    */
-    // console.log(yesno)
-    res.render('report', { title: 'report2101100142', answer: yesno.answer, imgSrc: yesno.image })
+router.get('/', (req, res) => {
+    https.get('https://yesno.wtf/api', (response) => {
+        response.on('data', (d) => {
+            const json = JSON.parse(d.toString('utf8'))
+            console.log(json.answer)
+            res.render('report', { title: 'report2101100142', answer: json.answer, imgSrc: json.image })
+        })
+    }).on('error', (e) => {
+        console.log(e)
+        res.send('error occurred')
+    })
 })
 
 module.exports = router
